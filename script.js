b34f75de229229f8665be6e8985228ca501e2399
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2025-09-18 21:15:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for sidebar links
+document.querySelectorAll('.sidebar a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
